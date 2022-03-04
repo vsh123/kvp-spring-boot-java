2bf123732f917b25482e657612552febdc508653
반선호 <shakevan@woowahan.com>
2022-03-04 23:17:29 +0900

    feat : http2까지 적용 및 빌드 해보기

--- kvp-spring-boot-web-undertow/src/main/java/kvp/van/tomcat/UndertowApplication.java
+++ kvp-spring-boot-web-undertow/src/main/java/kvp/van/tomcat/UndertowApplication.java
@@ -1,11 +1,42 @@
 package kvp.van.tomcat;
 
+import io.undertow.servlet.api.SecurityConstraint;
+import io.undertow.servlet.api.SecurityInfo;
+import io.undertow.servlet.api.TransportGuaranteeType;
+import io.undertow.servlet.api.WebResourceCollection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UndertowApplication {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(UndertowApplication.class);
+    }
+
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        final UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+        factory.addBuilderCustomizers(builder ->
+            builder.addHttpListener(8080, "0.0.0.0")
+        );
+
+        factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo.addSecurityConstraint(
+                new SecurityConstraint()
+                    .addWebResourceCollection(new WebResourceCollection().addUrlPattern("/*"))
+                    .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
+                    .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
+                    .setConfidentialPortManager(exchange -> 8443));
+        return factory;
     }
 }
