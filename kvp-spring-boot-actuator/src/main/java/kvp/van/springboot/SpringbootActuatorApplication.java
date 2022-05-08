1334230fc43413e5fd343b0312593911b54e5cc1
반선호 <shakevan@woowahan.com>
2022-05-08 15:23:33 +0900

    week 10 : spring-boot-actuator

---
+++ kvp-spring-boot-actuator/src/main/java/kvp/van/springboot/SpringbootActuatorApplication.java
@@ -0,0 +1,12 @@
+package kvp.van.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActuatorApplication.class, args);
+    }
+}
